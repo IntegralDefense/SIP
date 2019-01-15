3e47d4f794bf790db0ae3ac6c80be23e065a9e0b
Matthew Wilson <automationator@runbox.com>
2019-01-15 13:04:02 -0500

    Adds malware API

--- services/web/project/api/__init__.py
+++ services/web/project/api/__init__.py
@@ -17,6 +17,7 @@ from project.api.routes import indicator_relationship
 from project.api.routes import indicator_status
 from project.api.routes import indicator_type
 
+from project.api.routes import malware
 from project.api.routes import malware_type
 
 from project.api.routes import tag
