6b71ca5366731fc1558577068a53baa4daf7febd
Matthew Wilson <automationator@runbox.com>
2019-02-07 16:42:07 -0500

    Adds Indicator relationship API

--- services/web/project/api/routes/indicator_relationship.py
+++ services/web/project/api/routes/indicator_relationship.py
@@ -1,8 +1,6 @@
-from flask import jsonify, request
-
 from project import db
 from project.api import bp
-from project.api.decorators import check_if_token_required
+from project.api.decorators import check_if_token_required, validate_schema
 from project.api.errors import error_response
 from project.models import Indicator
 
@@ -10,34 +8,32 @@ from project.models import Indicator
 CREATE
 """
 
+create_schema = {
+    'type': 'null',
+    'properties': {},
+    'additionalProperties': False
+}
 
-@bp.route('/indicators/relationship', methods=['POST'])
+
+@bp.route('/indicators/<int:parent_id>/<int:child_id>/relationship', methods=['POST'])
 @check_if_token_required
-def create_indicator_relationship():
+@validate_schema(create_schema)
+def create_indicator_relationship(parent_id, child_id):
     """ Creates a parent/child relationship between two indicators """
-
-    data = request.values or {}
-
-    # Verify the required fields (parent_id and child_id) are present.
-    if 'parent_id' not in data or 'child_id' not in data:
-        return error_response(400, 'Request must include "parent_id" and "child_id"')
-
-    parent_id = data['parent_id']
-    child_id = data['child_id']
 
     # Verify the parent ID exists.
     parent_indicator = Indicator.query.get(parent_id)
     if not parent_indicator:
-        return error_response(404, 'Parent indicator not found')
+        return error_response(404, 'Parent indicator ID not found: {}'.format(parent_id))
 
     # Verify the child ID exists.
     child_indicator = Indicator.query.get(child_id)
     if not child_indicator:
-        return error_response(404, 'Child indicator not found')
+        return error_response(404, 'Child indicator ID not found: {}'.format(child_id))
 
     # Verify the parent and child are not the same.
     if parent_id == child_id:
-        return error_response(400, 'Cannot add indicator to its own children')
+        return error_response(400, 'Cannot add an indicator to its own children')
 
     # Try to create the relationship or error if it could not be created.
     result = parent_indicator.add_child(child_indicator)
@@ -45,6 +41,7 @@ def create_indicator_relationship():
         db.session.commit()
         return '', 204
     else:
+        db.session.rollback()
         return error_response(400, 'Child indicator already has a parent')
 
 
@@ -53,29 +50,20 @@ DELETE
 """
 
 
-@bp.route('/indicators/relationship', methods=['DELETE'])
+@bp.route('/indicators/<int:parent_id>/<int:child_id>/relationship', methods=['DELETE'])
 @check_if_token_required
-def delete_indicator_relationship():
+def delete_indicator_relationship(parent_id, child_id):
     """ Deletes an indicator parent/child relationship. """
-
-    data = request.values or {}
-
-    # Verify the required fields (parent_id and child_id) are present.
-    if 'parent_id' not in data or 'child_id' not in data:
-        return error_response(400, 'Request must include "parent_id" and "child_id"')
-
-    parent_id = data['parent_id']
-    child_id = data['child_id']
 
     # Verify the parent ID exists.
     parent_indicator = Indicator.query.get(parent_id)
     if not parent_indicator:
-        return error_response(404, 'Parent indicator not found')
+        return error_response(404, 'Parent indicator ID not found: {}'.format(parent_id))
 
     # Verify the child ID exists.
     child_indicator = Indicator.query.get(child_id)
     if not child_indicator:
-        return error_response(404, 'Child indicator not found')
+        return error_response(404, 'Child indicator ID not found: {}'.format(child_id))
 
     # Verify the parent and child are not the same.
     if parent_id == child_id:
@@ -86,4 +74,5 @@ def delete_indicator_relationship():
         db.session.commit()
         return '', 204
     else:
+        db.session.rollback()
         return error_response(400, 'Relationship does not exist')
