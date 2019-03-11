1f00a4fbdb358a3781f8baa62c675a84da188cbf
Matthew Wilson <automationator@runbox.com>
2019-03-11 13:54:42 -0400

    Fixes compatibility in initialize scripts

--- bin/initialize-PROD.sh
+++ bin/initialize-PROD.sh
@@ -2,7 +2,7 @@
 
 docker-compose -f docker-compose-PROD.yml up -d > /dev/null
 
-until $(curl --silent --head --insecure --request GET https://localhost:443 | grep "302 FOUND" > /dev/null); do
+until $(curl --silent --head --insecure --request GET https://127.0.0.1:443 | grep "302 FOUND" > /dev/null); do
     echo "Waiting for SIP (PROD) to start..."
     sleep 1
 done
--- bin/initialize-TEST.sh
+++ bin/initialize-TEST.sh
@@ -2,7 +2,7 @@
 
 docker-compose -f docker-compose-TEST.yml up -d > /dev/null
 
-until $(curl --silent --head --insecure --request GET https://localhost:4444 | grep "302 FOUND" > /dev/null); do
+until $(curl --silent --head --insecure --request GET https://127.0.0.1:4444 | grep "302 FOUND" > /dev/null); do
     echo "Waiting for SIP (TEST) to start..."
     sleep 1
 done
